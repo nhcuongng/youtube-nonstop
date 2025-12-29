855568a5c76cdafea9208a4469b749654d2b0000
Mason Nguyen <33181010+nhcuongng@users.noreply.github.com>
2025-12-29 17:33:16 +0700

    Improve auto-click functionality for confirm button
    Refactor mutation observer to auto-click confirm button

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-// Select the node that will be observed for mutations
-const targetNode = document.getElementsByTagName("ytd-popup-container")[0];
-
-// Options for the observer (which mutations to observe)
-const config = { childList: true };
+let intervalTimer = null;
 
 let count = 0;
 
@@ -11,26 +7,38 @@ let buttonConfirm = null;
 // Callback function to execute when mutations are observed
 const callback = (mutationList, observer) => {
   console.log('mutation', mutationList)
-  setTimeout(() => {
+  intervalTimer = setInterval(() => {
     if (!buttonConfirm) {
       buttonConfirm = document.querySelector("#confirm-button > yt-button-shape > button > yt-touch-feedback-shape");
       console.log('buttonConfirm', buttonConfirm);
-      observer.disconnect();
     }
     
     // for (const mutation of mutationList) {
     if (buttonConfirm) {
         buttonConfirm.click();
-        observer.observe(buttonConfirm, { attributes: true });
+        clearInterval(intervalTimer);
+        observer.disconnect();
+        observerButtonAutoClick.observe(buttonConfirm, { attributes: true });
         console.log('continue', count);
         count += 1;
     }
     // }
-  }, 500)
+  }, 200)
 };
 
 // Create an observer instance linked to the callback function
 const observer = new MutationObserver(callback);
 
 // Start observing the target node for configured mutations
-observer.observe(targetNode, config);
+observer.observe(
+  document.getElementsByTagName("ytd-popup-container")[0],
+  { childList: true }
+);
+
+const buttonAutoClick = () => {
+  buttonConfirm.click();
+  console.log('button auto click');
+}
+
+const observerButtonAutoClick = new MutationObserver(buttonAutoClick);
+
